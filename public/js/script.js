a98c0d2f7b6ada551c5d40d093ae13f7befe6fe3
Matic  Bernik <mb2551@student.uni-lj.si>
2016-04-06 14:54:02 +0000

    Prikaz podrobnosti seznama predvajanj

--- public/js/script.js
+++ public/js/script.js
@@ -31,5 +31,13 @@ $(document).ready(function() {
     });
   });
   
-  
+  $('#playlists div span').click(function(vnos){
+    //vnos.innerHTML=vnos.innerHTML+'fjkajkfjkasdjfnsadjfnsan';
+    //$(this).text($(this).text()+" fasfasdfjanfjkas");
+    var details = $(this);
+    $.get('/playlist/' + details.parent().attr('playlist'), function(data) {
+      details.html(' (' + data.tracks + ' track' + (data.tracks != 1? 's': '') + ' | ' +
+        Math.round(data.time / 60000) + ' min | <strong>$' + Math.round(data.price) + '</strong> total)');
+    });  
+  });
 });
